b0d98e03aa1105a7bda614fe853235700ec9cac2
kiran K Kharvi <kirankharvi22@gmail.com>
2019-06-20 12:44:04 +0530

    in branch DS

--- data_structure/list2(unpacking).py
+++ data_structure/list2(unpacking).py
@@ -1,5 +1,6 @@
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-first, second, third, *other, last = number
+first, second, third, *other, lastbutone, last = number
 print(first)
 print(first, last)
 print(other)
+print(lastbutone)
