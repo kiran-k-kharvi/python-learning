62580b029f64a960736bbf0e9f23ba0f6c0c164d
kiran K Kharvi <kirankharvi22@gmail.com>
2019-06-20 12:12:07 +0530

    in branch of key

---
+++ data_structure/list(add&rmv_itrm).py
@@ -0,0 +1,19 @@
+letters = ["a", "b", "c"]
+
+print(letters)
+# add
+letters.append("d")
+print(letters)
+
+letters.insert(0, "-")
+print(letters)
+
+# remove
+letters.pop()
+print(letters)
+
+letters.pop(0)
+print(letters)
+
+letters.pop(1)
+print(letters)
---
+++ data_structure/list(loop).py
@@ -0,0 +1,5 @@
+letters = ["a", "b", "c"]
+for x in letters:
+    print(x)
+for index, letter in enumerate(letters):
+    print(index, letter)
